affadb95076e0b40a956387303265f6cba2708e8
Subham Panda <subhampanda2899@gmail.com>
2020-09-09 19:29:39 +0530

    Frontend done

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyClTBqainsov4ey_x-qPVPXOU3xQCdqeKQ",
+    authDomain: "clone-662ae.firebaseapp.com",
+    databaseURL: "https://clone-662ae.firebaseio.com",
+    projectId: "clone-662ae",
+    storageBucket: "clone-662ae.appspot.com",
+    messagingSenderId: "376480325535",
+    appId: "1:376480325535:web:37cdcfdac8ed074382a7aa",
+    measurementId: "G-X7X8ENLSPR"
+};
